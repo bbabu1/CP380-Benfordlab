892fcbc064fbaec283bc30950260e0ba4a05b689
BONY BABU <65872018+bbabu1@users.noreply.github.com>
2021-08-02 19:58:58 -0400

    Add files via upload

---
+++ BenfordLab/Benford.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace BenfordLab
+{
+    public class BenfordData
+    {
+        public int Digit { get; set; }
+        public int Count { get; set; }
+
+        public BenfordData() { }
+    }
+
+    public class Benford
+    {
+
+        public static BenfordData[] calculateBenford(string csvFilePath)
+        {
+            // load the data
+            var data = File.ReadAllLines(csvFilePath)
+                .Skip(1) // For header
+                .Select(s => Regex.Match(s, @"^(.*?),(.*?)$"))
+                .Select(data => new
+                {
+                    Country = data.Groups[1].Value,
+                    Population = int.Parse(data.Groups[2].Value)
+                });
+
+            // manipulate the data!
+            //
+            // Select() with:
+            //   - Country
+            //   - Digit (using: FirstDigit.getFirstDigit() )
+            // 
+            var m_data = data
+                .Select(m_data => new 
+                {
+                    Country = m_data.Country, 
+                    Digit= FirstDigit.getFirstDigit(m_data.Population) 
+                });
+
+            // Then:
+            //   - you need to count how many of *each digit* there are
+            //
+
+            Func<int, int> countDigit = i =>
+            {
+                var k = 0;
+                foreach (var item in m_data)
+                {
+                    if (i == item.Digit)
+                    {
+                        k++;
+                    }
+                }
+                return k;
+            };
+
+            int[] count= new int[9];
+            int[] digit = new int[9];
+            for(var i=0;i<9;i++)
+            {
+
+                digit[i] = i + 1;
+                count[i] = countDigit(i+1);
+            }
+            
+            int temp,t;
+            for (int j = 0; j <= count.Length - 2; j++)
+            {
+                for (int i = 0; i <= count.Length - 2; i++)
+                {
+                    if (count[i] > count[i + 1])
+                    {
+                        temp = count[i + 1];
+                        count[i + 1] = count[i];
+                        count[i] = temp;
+                        t = digit[i + 1];
+                        digit[i + 1] = digit[i];
+                        digit[i] = t;
+
+
+                    }
+                }
+            }
+            Console.WriteLine("--------------");
+            // Lastly:
+            //   - transform (select) the data so that you have a list of
+            //     BenfordData objects
+            //
+
+            BenfordData[] b = new BenfordData[9];
+            for (var i = 0; i < 9; i++)
+            {
+                BenfordData bo = new BenfordData()
+                {
+                    Count = count[i],
+                    Digit = digit[i]
+
+                };
+                b[i] = bo;
+            }
+              var m = b  ;
+              return m.ToArray();
+
+        }
+
+       
+    }
+}
---
+++ BenfordLab/FirstDigit.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenfordLab
+{
+    public class FirstDigit
+    {
+        /* Given an integer X, this method returns the first digit
+         * 
+         * For example,
+         *   5 ==> returns 5
+         *   10 ==> returns 1
+         *   654321 ==> returns 6
+         */
+        public static int getFirstDigit(int digit) {
+            int num = digit;
+            while(num>=10)
+            {
+                num = num / 10;
+            }
+            /*int count = 0;
+            int div = 1;
+            while(num > 0)
+            {
+                count++;
+                num = num / 10;
+            }
+            for(int i=0; i<(count-1);i++)
+            {
+                div = div * 10;
+            }
+            return digit / div;*/
+            // TODO
+            return num;
+        }    
+    }
+}
