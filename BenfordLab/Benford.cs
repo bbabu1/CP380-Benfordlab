78893c30b75f7f479a3a9231d4e0dbda2a50fe8a
BONY BABU <65872018+bbabu1@users.noreply.github.com>
2021-08-02 20:08:28 -0400

    changed some  errors

--- BenfordLab/Benford.cs
+++ BenfordLab/Benford.cs
@@ -4,65 +4,46 @@ using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace BenfordLab
-{
-    public class BenfordData
+{ public class BenfordData
     {
-        public int Digit { get; set; }
-        public int Count { get; set; }
-
-        public BenfordData() { }
+     public int Digit { get; set; }
+     public int Count { get; set; }
+     public BenfordData() { }
     }
-
-    public class Benford
+ public class Benford
     {
-
-        public static BenfordData[] calculateBenford(string csvFilePath)
+     public static BenfordData[] calculateBenford(string csvFilePath)
         {
-            // load the data
-            var data = File.ReadAllLines(csvFilePath)
-                .Skip(1) // For header
-                .Select(s => Regex.Match(s, @"^(.*?),(.*?)$"))
-                .Select(data => new
-                {
-                    Country = data.Groups[1].Value,
-                    Population = int.Parse(data.Groups[2].Value)
-                });
-
-            // manipulate the data!
-            //
-            // Select() with:
-            //   - Country
-            //   - Digit (using: FirstDigit.getFirstDigit() )
-            // 
-            var m_data = data
-                .Select(m_data => new 
-                {
-                    Country = m_data.Country, 
-                    Digit= FirstDigit.getFirstDigit(m_data.Population) 
-                });
-
-            // Then:
-            //   - you need to count how many of *each digit* there are
-            //
-
-            Func<int, int> countDigit = i =>
+          var data = File.ReadAllLines(csvFilePath)
+          .Skip(1) 
+          .Select(s => Regex.Match(s, @"^(.*?),(.*?)$"))
+          .Select(data => new
+        {
+          Country = data.Groups[1].Value,
+          Population = int.Parse(data.Groups[2].Value)
+        });
+         var m_data = data
+         .Select(m_data => new 
+             {
+             Country = m_data.Country, 
+             Digit= FirstDigit.getFirstDigit(m_data.Population) 
+             });
+             Func<int, int> countDigit = i =>
             {
-                var k = 0;
-                foreach (var item in m_data)
+            var k = 0;
+            foreach (var item in m_data)
                 {
-                    if (i == item.Digit)
-                    {
-                        k++;
-                    }
+                 if (i == item.Digit)
+                {
+                 k++;
+                }
                 }
                 return k;
-            };
-
-            int[] count= new int[9];
-            int[] digit = new int[9];
-            for(var i=0;i<9;i++)
+                };
+                int[] count= new int[9];
+                int[] digit = new int[9];
+                for(var i=0;i<9;i++)
             {
-
                 digit[i] = i + 1;
                 count[i] = countDigit(i+1);
             }
@@ -70,9 +51,9 @@ namespace BenfordLab
             int temp,t;
             for (int j = 0; j <= count.Length - 2; j++)
             {
-                for (int i = 0; i <= count.Length - 2; i++)
-                {
-                    if (count[i] > count[i + 1])
+            for (int i = 0; i <= count.Length - 2; i++)
+            {
+           if (count[i] > count[i + 1])
                     {
                         temp = count[i + 1];
                         count[i + 1] = count[i];
@@ -80,33 +61,22 @@ namespace BenfordLab
                         t = digit[i + 1];
                         digit[i + 1] = digit[i];
                         digit[i] = t;
-
-
-                    }
-                }
-            }
+                   }
+                   }
+                   }
             Console.WriteLine("--------------");
-            // Lastly:
-            //   - transform (select) the data so that you have a list of
-            //     BenfordData objects
-            //
-
             BenfordData[] b = new BenfordData[9];
             for (var i = 0; i < 9; i++)
             {
-                BenfordData bo = new BenfordData()
+            BenfordData bo = new BenfordData()
                 {
-                    Count = count[i],
-                    Digit = digit[i]
-
+                Count = count[i],
+                Digit = digit[i]
                 };
                 b[i] = bo;
             }
               var m = b  ;
               return m.ToArray();
-
-        }
-
-       
-    }
-}
+            }
+            }
+            }
